625f2d62e9caee0131111eebe77b23dfe609f704
juan <maxdarkx@gmail.com>
2022-02-10 19:55:37 -0500

    se impriment en pantalla las dos barras (raquetas) que utilizara el juego

--- main.js
+++ main.js
@@ -13,11 +13,36 @@ class Board
 
     get elements()
     {
-        var elements = this.bars;
-        elements.push(ball);
+        let elements = this.bars.map(function(bar){return bar;});
+        elements.push(this.ball);
         return elements;
     }
 }
+
+class Bar
+{
+    constructor(x, y, width, height, board)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+
+    down()
+    {
+
+    }
+
+    up()
+    {
+
+    }
+}
+
 
 class BoardView
 {
@@ -25,10 +50,33 @@ class BoardView
     constructor(canvas, board)
     {
         this.canvas = canvas;
+        this.board = board;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
-        this.board = board;
         this.context = canvas.getContext("2d");
+
+    }
+
+    drawBoard()
+    {
+        for(var i = this.board.elements.length - 1; i >= 0; i--)
+        {
+            var elementToDraw = this.board.elements[i];
+            this.drawElement(this.context, elementToDraw);
+        }
+    }
+
+    drawElement(context, element)
+    {
+        if(element != null){
+            switch(element.kind)
+            {
+                case "rectangle":
+                    
+                    context.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
 
     }
 }
@@ -38,7 +86,10 @@ window.addEventListener("load", main);
 
 function main()
 {
-    var board = new Board(800, 400);
     var canvas = document.getElementById('canvas');
+    var board = new Board(800, 400);
+    var bar1 = new Bar(20, 100, 40, 100, board);
+    var bar2 = new Bar(740, 100, 40, 100, board);
     var boardView = new BoardView(canvas, board);
+    boardView.drawBoard();
 }
